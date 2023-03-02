e5c504e5e34e044c156f01ce5484fb07afe9692e
ixxxl <ixxxl@mail.ru>
2023-03-02 19:59:46 +0200

    6575

---
+++ src/components/form1Component.tsx
@@ -0,0 +1,18 @@
+export default function form1Component() {
+  return (
+    <div className="form">
+      <h2>НАПИШИТЕ НАМ</h2>
+      <div className="form-block">
+        <form method="GET">
+          <input
+            type="text"
+            className="imput_txt"
+            name="name"
+            placeholder="Имя"
+          />
+          <button className="button">отправить</button>
+        </form>
+      </div>
+    </div>
+  );
+}
